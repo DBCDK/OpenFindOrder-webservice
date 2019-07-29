f82de5d0cf3f9ec19961b7ecee014c8c16706f56
Jørgen G. Nielsen <jgn@dbc.dk>
2019-07-29 16:11:44 +0200

    39: update Jenkinsfile

--- jenkins/scripts/utilities.groovy
+++ jenkins/scripts/utilities.groovy
@@ -40,7 +40,7 @@ def  copyDockerFiles(String version = '2.5') {
 
     // Make index.php symbolic link.
     dir("docker/webservice") {
-        if (${version}.is('2.6')) {
+        // if (${version}.equals('2.6')) {
             def SED_STRING = '''
                 DIR=$APACHE_ROOT/2.6
                 INI=$DIR/openfindorder.ini
@@ -57,7 +57,7 @@ def  copyDockerFiles(String version = '2.5') {
                     sed -i "s#@LOGFILE@#$LOGFILE#g" $INI
                     sed -i "s#@VERBOSE_LEVEL@#$VERBOSE_LEVEL#g" $INI
                 fi'''
-        }
+        // }
         sh """
             print 'testhest'
             // sed -i "s#@RUN-OFO@#${SED_STRING}#g" run-ofo.sh
