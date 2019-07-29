fab52cab7b3322c4f74086b75f59b8105eb45ab1
Jørgen G. Nielsen <jgn@dbc.dk>
2019-07-29 15:54:50 +0200

    39: update Jenkinsfile

--- jenkins/scripts/utilities.groovy
+++ jenkins/scripts/utilities.groovy
@@ -41,22 +41,22 @@ def  copyDockerFiles(String version = '2.5') {
     // Make index.php symbolic link.
     dir("docker/webservice") {
         if (${version} == '2.6') {
-            def SED_STRING = "
-                DIR=$APACHE_ROOT/" + ${version} + "
+            def SED_STRING = '''
+                DIR=$APACHE_ROOT/''' + ${version} + '''
                 INI=$DIR/openfindorder.ini
-                  INSTALL=$INI\"_INSTALL\"
+                INSTALL=$INI"_INSTALL"
                 cp $DIR/openfindorder.wsdl_INSTALL $DIR/openfindorder.wsdl
                 if [ ! -f $INI ] ; then
                     cp $INSTALL $INI
-                    sed -i \"s#@OPENAGENCY_AGENCY_LIST@#$OPENAGENCY_AGENCY_LIST_PROD#g\" $INI
-                    sed -i \"s#@ORS2_URL@#$ORS2_URL_PROD#g\" $INI
-                    sed -i \"s#@CACHE_SETTINGS@#$CACHE_SETTINGS#g\" $INI
-                    sed -i \"s#@MY_DOMAIN@#$MY_DOMAIN#g\" $INI
-                    sed -i \"s#@MY_DOMAIN_IP_LIST@#$MY_DOMAIN_IP_LIST#g\" $INI
-                    sed -i \"s#@AAA_FORS_RIGHTS@#$AAA_FORS_RIGHTS#g\" $INI
-                    sed -i \"s#@LOGFILE@#$LOGFILE#g\" $INI
-                    sed -i \"s#@VERBOSE_LEVEL@#$VERBOSE_LEVEL#g\" $INI
-                fi"
+                    sed -i "s#@OPENAGENCY_AGENCY_LIST@#$OPENAGENCY_AGENCY_LIST_PROD#g" $INI
+                    sed -i "s#@ORS2_URL@#$ORS2_URL_PROD#g" $INI
+                    sed -i "s#@CACHE_SETTINGS@#$CACHE_SETTINGS#g" $INI
+                    sed -i "s#@MY_DOMAIN@#$MY_DOMAIN#g" $INI
+                    sed -i "s#@MY_DOMAIN_IP_LIST@#$MY_DOMAIN_IP_LIST#g" $INI
+                    sed -i "s#@AAA_FORS_RIGHTS@#$AAA_FORS_RIGHTS#g" $INI
+                    sed -i "s#@LOGFILE@#$LOGFILE#g" $INI
+                    sed -i "s#@VERBOSE_LEVEL@#$VERBOSE_LEVEL#g" $INI
+                fi'''
         }
         sh """
             print "Parameter: SED_STRING = ${SED_STRING}"
