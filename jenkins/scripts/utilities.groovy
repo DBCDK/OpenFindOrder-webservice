d93cab5cc906929c3eb08fff766216a7b67d60fa
Jørgen G. Nielsen <jgn@dbc.dk>
2019-07-29 16:18:42 +0200

    39: update Jenkinsfile

--- jenkins/scripts/utilities.groovy
+++ jenkins/scripts/utilities.groovy
@@ -42,25 +42,25 @@ def  copyDockerFiles(String version = '2.5') {
     dir("docker/webservice") {
         // if (${version}.equals('2.6')) {
             def SED_STRING = '''
-                DIR=$APACHE_ROOT/2.6
-                INI=$DIR/openfindorder.ini
-                INSTALL=$INI"_INSTALL"
-                cp $DIR/openfindorder.wsdl_INSTALL $DIR/openfindorder.wsdl
-                if [ ! -f $INI ] ; then
-                    cp $INSTALL $INI
-                    sed -i "s#@OPENAGENCY_AGENCY_LIST@#$OPENAGENCY_AGENCY_LIST_PROD#g" $INI
-                    sed -i "s#@ORS2_URL@#$ORS2_URL_PROD#g" $INI
-                    sed -i "s#@CACHE_SETTINGS@#$CACHE_SETTINGS#g" $INI
-                    sed -i "s#@MY_DOMAIN@#$MY_DOMAIN#g" $INI
-                    sed -i "s#@MY_DOMAIN_IP_LIST@#$MY_DOMAIN_IP_LIST#g" $INI
-                    sed -i "s#@AAA_FORS_RIGHTS@#$AAA_FORS_RIGHTS#g" $INI
-                    sed -i "s#@LOGFILE@#$LOGFILE#g" $INI
-                    sed -i "s#@VERBOSE_LEVEL@#$VERBOSE_LEVEL#g" $INI
+                DIR=\$APACHE_ROOT/2.6
+                INI=\$DIR/openfindorder.ini
+                INSTALL=\$INI"_INSTALL"
+                cp \$DIR/openfindorder.wsdl_INSTALL \$DIR/openfindorder.wsdl
+                if [ ! -f \$INI ] ; then
+                    cp \$INSTALL \$INI
+                    sed -i "s#@OPENAGENCY_AGENCY_LIST@#\$OPENAGENCY_AGENCY_LIST_PROD#g" \$INI
+                    sed -i "s#@ORS2_URL@#\$ORS2_URL_PROD#g" \$INI
+                    sed -i "s#@CACHE_SETTINGS@#\$CACHE_SETTINGS#g" \$INI
+                    sed -i "s#@MY_DOMAIN@#\$MY_DOMAIN#g" \$INI
+                    sed -i "s#@MY_DOMAIN_IP_LIST@#\$MY_DOMAIN_IP_LIST#g" \$INI
+                    sed -i "s#@AAA_FORS_RIGHTS@#\$AAA_FORS_RIGHTS#g" \$INI
+                    sed -i "s#@LOGFILE@#v$LOGFILE#g" \$INI
+                    sed -i "s#@VERBOSE_LEVEL@#\$VERBOSE_LEVEL#g" \$INI
                 fi'''
         // }
         sh """
-            echo SED_STRING
-            // sed -i "s#@RUN-OFO@#${SED_STRING}#g" run-ofo.sh
+            echo ${SED_STRING}
+            sed -i "s#@RUN-OFO@#${SED_STRING}#g" run-ofo.sh
             """
     }
 }
