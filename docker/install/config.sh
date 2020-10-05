d8068c20f0dee98b237a5698343e3db1aae44f10
Jørgen G. Nielsen <jgn@dbc.dk>
2020-10-05 14:44:09 +0200

    replace test/next url

--- docker/install/config.sh
+++ docker/install/config.sh
@@ -34,7 +34,7 @@ do
     if [ ! -f $INI ] ; then
         cp $INSTALL $INI
         sed -i "s#@OPENAGENCY_AGENCY_LIST@#$OPENAGENCY_AGENCY_LIST_STAGING#g" $INI
-        sed -i "s#@ORS2_URL@#$ORS2_URL_STAGING#g" $INI
+        sed -i "s#@ORS2_URL@#$ORS2_URL_NEXT#g" $INI
         sed -i "s#@CACHE_SETTINGS@#$CACHE_SETTINGS#g" $INI
         sed -i "s#@MY_DOMAIN@#$MY_DOMAIN#g" $INI
         sed -i "s#@MY_DOMAIN_IP_LIST@#$MY_DOMAIN_IP_LIST#g" $INI
@@ -56,7 +56,7 @@ do
     if [ ! -f $INI ] ; then
         cp $INSTALL $INI
         sed -i "s#@OPENAGENCY_AGENCY_LIST@#$OPENAGENCY_AGENCY_LIST_PROD#g" $INI
-        sed -i "s#@ORS2_URL@#$ORS2_URL_STAGING#g" $INI
+        sed -i "s#@ORS2_URL@#$ORS2_URL_TEST#g" $INI
         sed -i "s#@CACHE_SETTINGS@#$CACHE_SETTINGS#g" $INI
         sed -i "s#@MY_DOMAIN@#$MY_DOMAIN#g" $INI
         sed -i "s#@MY_DOMAIN_IP_LIST@#$MY_DOMAIN_IP_LIST#g" $INI
